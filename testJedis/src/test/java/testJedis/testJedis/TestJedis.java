5abb9a03fdab11bca0e99a71f82d1e9d0635c986
86198 <86198@LAPTOP-BOLICOH3>
2021-09-21 11:22:11 +0800

    test

---
+++ testJedis/src/test/java/testJedis/testJedis/TestJedis.java
@@ -0,0 +1,13 @@
+package testJedis.testJedis;
+
+import redis.clients.jedis.Jedis;
+
+public class TestJedis {
+	public static void main(String[] args) {
+		Jedis jedis = new Jedis("localhost");//localhost可以换为其他url接口
+		jedis.set("你好~", "中秋节快乐！");//赋值，将"你好~"的值设置为 "中秋节快乐！"
+		String value = jedis.get("你好~");
+		System.out.print(value);
+	}
+
+}
